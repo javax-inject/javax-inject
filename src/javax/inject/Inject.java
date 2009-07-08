10c91471020775c266680402851922ff98e4be63
crazyboblee <crazyboblee@3bc8319c-20ab-11de-9edc-3f40a397ab60>
2009-07-08 23:24:37 +0000

    s/prevent/preclude/

--- src/javax/inject/Inject.java
+++ src/javax/inject/Inject.java
@@ -110,7 +110,7 @@ import static java.lang.annotation.ElementType.FIELD;
  * <p>Injection of members annotated with {@code @Inject} is required. While an
  * injectable member may use any accessibility modifier (including
  * <tt>private</tt>), platform or injector limitations (like security
- * restrictions or lack of reflection support) might prevent injection
+ * restrictions or lack of reflection support) might preclude injection
  * of non-public members.
  *
  * <h3>Qualifiers</h3>
