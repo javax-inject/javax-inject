52963929873fbd79adccd8a4b8f7bb1a09cc9a6f
crazyboblee <crazyboblee@3bc8319c-20ab-11de-9edc-3f40a397ab60>
2009-09-14 03:09:36 +0000

    Updated build script for PFD.

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,5 +1,4 @@
-VERSION=EDR
-NAME=atinject
+NAME=javax.inject
 
 # Clear and recreate build directory.
 rm -r build
@@ -9,22 +8,21 @@ mkdir -p build/dist
 # Compile classes.
 javac -g -d build/classes `find src -name *.java`
 
+FOOTER="<font size='-1'>Copyright (C) 2009 <a href='http://code.google.com/p/atinject/'>\
+The JSR-330 Expert Group</a>. \
+Licensed under the <a href='http://www.apache.org/licenses/LICENSE-2.0'>Apache \
+License</a>, Version 2.0.</font>"
+
 # Generate Javadocs.
-javadoc -protected \
-            -header "<font color='red'><b>This is a DRAFT specification.</b></font>" \
-            -sourcepath src -d build/javadoc javax.inject
+javadoc -protected -bottom "$FOOTER" \
+	-sourcepath src -d build/javadoc javax.inject
 
 # Generate jars.
 cp -R src build
 find build/src -name .svn -type d | xargs rm -r
-jar cfM build/dist/$NAME-$VERSION-src.zip -C build/src .
+jar cfM build/dist/$NAME-src.zip -C build/src .
 
-jar cfM build/dist/$NAME-$VERSION-javadoc.zip -C build/javadoc .
-jar cfM build/dist/$NAME-$VERSION.jar -C build/classes .
+jar cfM build/dist/$NAME-javadoc.zip -C build/javadoc .
+jar cfM build/dist/$NAME.jar -C build/classes .
 
-jar cfM build/$NAME-$VERSION.zip -C build/dist .
-
-# Pending license footer.
-#-bottom <font size='-1'>Copyright (C) 2009 The JSR-330 Expert Group. Licensed 
-# under the <a href='http://www.apache.org/licenses/LICENSE-2.0'>Apache License</a>,
-#            Version 2.0.</font>
+jar cfM build/$NAME.zip -C build/dist .
