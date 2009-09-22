7f0aebd57afde7462247865dbb72560afad9e0fe
crazyboblee <crazyboblee@3bc8319c-20ab-11de-9edc-3f40a397ab60>
2009-09-22 00:02:51 +0000

    Added small example to @Named.

--- src/javax/inject/Named.java
+++ src/javax/inject/Named.java
@@ -22,6 +22,15 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * String-based {@linkplain Qualifier qualifier}.
+ *
+ * <p>Example usage:
+ *
+ * <pre>
+ *   public class Car {
+ *     &#064;Inject <b>@Named("driver")</b> Seat driverSeat;
+ *     &#064;Inject <b>@Named("passenger")</b> Seat passengerSeat;
+ *     ...
+ *   }</pre>
  */
 @Qualifier
 @Documented
